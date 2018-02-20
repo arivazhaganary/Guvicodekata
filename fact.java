c7ed620985e0a2868bfb5e7367065f2533298349
arivazhaganary <arivazhagan30071997@gmail.com>
2018-02-20 21:13:47 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class fact{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int fact = 1;
+		for(int i = 1;i<=a;i++){
+			fact = fact*i;
+		}
+		System.out.println(fact);
+	}
+}
